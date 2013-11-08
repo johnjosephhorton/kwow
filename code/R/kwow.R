0a7b63531687468a29fd70bfe31247ae41a7af7a
redmode <gedranovich@gmail.com>
2013-11-08 03:38:38 +0300

    Day #1

--- code/R/kwow.R
+++ code/R/kwow.R
@@ -4,7 +4,25 @@
 # Author: John J. Horton 
 # Created Date: 2013-11-07 
 # Project: kwow 
-###################################################
+###############################################
+
+setwd("./code/R")
+
+mturk.file <- "../../data/mturk_output.csv"
+mturk.df.raw <- read.table(mturk.file, header=T, sep=",", fill=T)
+mturk.df <- mturk.df.raw[, c("Input.Title",
+                             "Answer.know_job",
+                             "Answer.know_anyone",
+                             "Answer.wage",
+                             "Answer.volume_trend",
+                             "Answer.wage_trend",
+                             "Answer.comment")]
+
+tb <- table(mturk.df$Input.Title)
+jtitle <- names(tb[tb<30])
+
+mturk.df[mturk.df$Input.Title==jtitle,1]
+# Licensed Practical and Licensed Vocational Nurses
 
 library(ggplot2)
 library(scales)
